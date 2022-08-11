bb1c190e25f7fb6b41a30f1ea8b45d042cfa9fa5
Sabri <sabri.rizzo@gmail.com>
2022-08-11 12:09:49 +0200

    cliente

--- src/main/java/modelos/Pelicula.java
+++ src/main/java/modelos/Pelicula.java
@@ -1,11 +1,69 @@
 package modelos;
 
+import java.util.ArrayList;
+
 public class Pelicula {
     int id;
     String nombre;
     String genero;
-    String reparto;
-    int duracion;
+    ArrayList actoresReparto = new ArrayList();
+    float duracion;
     String formato;
     boolean alquilado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public ArrayList getActoresReparto() {
+        return actoresReparto;
+    }
+
+    public void setActoresReparto(ArrayList actoresReparto) {
+        this.actoresReparto = actoresReparto;
+    }
+
+    public float getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public boolean isAlquilado() {
+        return alquilado;
+    }
+
+    public void setAlquilado(boolean alquilado) {
+        this.alquilado = alquilado;
+    }
 }
