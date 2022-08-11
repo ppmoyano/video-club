c225a055c07fc14b4178f6fbb3a87168fe930ca5
Pato <patriciogranelli@gmail.com>
2022-08-11 20:49:43 +0200

    Hice el primer boceto del menú

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -26,15 +26,45 @@ do {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Ingrese el Apellido o el DNI: ");
-            System.out.println("Aca buscaria el DNI del cliente"); //Necesitaría más info para poder avanzar
-        // NO ENTENDÍ LO DEL DNI STRING
-        //NO SE COMO JUNTAR ESTO CON LA INFO DE LOS CLIENTES PARA SABER SI EXISTEN O NO.
+        String opcion = teclado.nextLine();
+        System.out.println("Lo siento no se encontre el Cliente desado.");  // Queda así hardocodeado solamente para el ejemplo, una vez que tengamos más info lo modifico bien.
     }
     private static void buscarPelicula() { //Acá se buscan las películas, con los géneros
         Scanner teclado = new Scanner(System.in);
         System.out.println("Desea buscar la película por:");
         System.out.println("1 - Titulo 2 - Genero - 3 - Reparto 0 - Volver");
-        // NO ENTENDÍ CÓMO SEGUIR ESTA PARTE
+        int opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1:
+                buscarPorTitulo();
+                break;
+            case 2:
+                buscarPorGenero();
+                break;
+            case 3:
+                buscarPorReparto();
+
+                break;
+        }
+    }
+
+    private static void buscarPorReparto() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el reparto:");
+        String opcionReparto = teclado.nextLine();
+    }
+
+    private static void buscarPorGenero() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el genero:");
+        String opcionGenero = teclado.nextLine();
+        System.out.println("Lo siento, no tenemos ninguna pelicula disponible en ese genero"); // Queda así hardocodeado solamente para el ejemplo, una vez que tengamos más info lo modifico bien.
+    }
+
+    private static void buscarPorTitulo() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el título:");
+        String opcionTitulo = teclado.nextLine();
     }
 
     private static int menu() {
