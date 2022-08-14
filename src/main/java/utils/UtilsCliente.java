d273083040ffacc494673ca031fda680ab019d0c
Sabri <sabri.rizzo@gmail.com>
2022-08-14 15:28:05 +0200

    cliente

--- src/main/java/utils/UtilsCliente.java
+++ src/main/java/utils/UtilsCliente.java
@@ -1,6 +1,24 @@
 package utils;
 
+import modelos.Cliente;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class UtilsCliente {
 
     public static Cliente buscarCliente(List<Cliente> listaDeClientes, String apellido) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el APELLIDO del Cliente");
+        String apellidoABuscar = teclado.next();
+        Cliente clienteEncontrado = new Cliente();
+        for (int i = 0; i < listaDeClientes.size(); i++) {
+            if (apellidoABuscar == listaDeClientes.get(i).getApellido()) {
+                clienteEncontrado=listaDeClientes.get(i);
+            }
+        }
+
+        return clienteEncontrado;
+    }
 }
