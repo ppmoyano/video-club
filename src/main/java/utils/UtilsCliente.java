f2f61725dae7352434ee5b8160138c06501ca421
Sabri <sabri.rizzo@gmail.com>
2022-08-16 12:51:57 +0200

    cliente

--- src/main/java/utils/UtilsCliente.java
+++ src/main/java/utils/UtilsCliente.java
@@ -10,11 +10,10 @@ public class UtilsCliente {
     public static Cliente buscarCliente(List<Cliente> listaDeClientes, String apellido) {
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese el APELLIDO del Cliente");
-        String apellidoABuscar = teclado.next();
+
         Cliente clienteEncontrado = new Cliente();
         for (int i = 0; i < listaDeClientes.size(); i++) {
-            if (apellidoABuscar == listaDeClientes.get(i).getApellido()) {
+            if (apellido == listaDeClientes.get(i).getApellido()) {
                 clienteEncontrado=listaDeClientes.get(i);
             }
         }
