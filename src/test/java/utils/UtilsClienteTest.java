ed8c808aaecea8ba29baeaa2fe26b96925fe1b98
Sabrina Rizzo <106767649+rizzosabri@users.noreply.github.com>
2022-08-25 15:52:51 +0200

    cliente (#10)
    * cliente
    
    * cliente

--- src/test/java/utils/UtilsClienteTest.java
+++ src/test/java/utils/UtilsClienteTest.java
@@ -23,17 +23,12 @@ public class UtilsClienteTest {
 
     @Test
     public void buscarClienteByApellido_return_clienteEsperado() {
-        Cliente cliente = UtilsCliente.buscarCliente(listaDeClientes, "Lopeza".substring(0, 5));
+        Cliente cliente = UtilsCliente.buscarCliente(listaDeClientes, "Lopeza".substring(0,5));
         String message = "Error de busqueda: Se busco por apellido Lopez y no encontro el cliente de apellido " + cliente.getApellido();
         assertEquals("Lopez", cliente.getApellido(), message);
     }
 
-    @Test
-    public void buscarClienteByApellido_return_clienteVacioSiElApellidoEsNull() {
-        Cliente cliente = UtilsCliente.buscarCliente(listaDeClientes, null);
-        String message = "Error de busqueda: Se busco por apellido null y no devolvio un cliente vacio ";
-        assertEquals(null, cliente.getApellido(), message);
-    }
+
 
     @Test
     public void buscarClienteByApellido_return_clienteVacioSiElApellidoNoSeEncuentra() {
@@ -43,18 +38,13 @@ public class UtilsClienteTest {
     }
 
     @Test
-    public void buscarClienteByDNI_return_clienteEsperado() {
+   public void buscarClienteByDNI_return_clienteEsperado() {
         Cliente cliente = UtilsCliente.buscarClientePorDni(listaDeClientes, "12".substring(0, 1));
         String message = "Error de busqueda: se busco DNI 1 y no lo encontro";
         assertEquals("1", cliente.getDni(), message);
     }
 
-    @Test
-    public void buscarClienteByDNI_return_clienteVacioSiElDNIesNull() {
-        Cliente cliente = UtilsCliente.buscarClientePorDni(listaDeClientes, null);
-        String message = "Error de busqueda: Se busco por dni= null y no devolvio un cliente vacio ";
-        assertEquals(null, cliente.getApellido(), message);
-    }
+
 
     @Test
     public void buscarClienteByDNI_return_clienteVacioSiElDNINoSeEncuentra() {
@@ -62,5 +52,6 @@ public class UtilsClienteTest {
         String message = "Error de busqueda: Se busco por apellido inexistente y no devolvio un cliente vacio ";
         assertEquals(null, cliente.getApellido(), message);
     }
+
 
 }
